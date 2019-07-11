6b084b947d27a6abf256c0a68075306185e5cd98
Valerii Zuev <valera.zuev.zva@gmail.com>
2019-07-11 10:35:39 +0300

    summer alg practice: report ready for 2/3

---
+++ tex/primat/algorithms/summer-practice-2019/code/graph.c
@@ -0,0 +1,20 @@
+#include "graph.h"
+int ** graphMemAlloc(int q) {
+    int ** graph = (int **)malloc(q * sizeof(int *));
+    for (int i = 0; i < q; i++)
+        graph[i] = (int *)malloc(q * sizeof(int) * VERTICES_IN_EDGE);
+    return graph;
+}
+void graphRead(int q, int ** graph, FILE * fin) {
+    int v, u;
+    for (int i = 0; i < q; i++) {
+        fscanf(fin, "%i %i\n", &v, &u);
+        graph[i][0] = v;
+        graph[i][1] = u;
+        printf("%i %i\n", graph[i][0], graph[i][1]);
+    }
+}
+void graphMemFree(int q, int ** graph) {
+    for (int i = 0; i < q; i++)
+        free(graph[i]);
+}
