c6a727fa9b216c34d713a1a4c3b576933e871a53
Valerii Zuev <valera.zuev.zva@gmail.com>
2019-11-22 06:55:18 +0300

    python:stepik: add concise gcd implementation

--- python/stepic_alg_csc/s362gcd.py
+++ python/stepic_alg_csc/s362gcd.py
@@ -3,9 +3,9 @@ import random as rand
 
 def test_gcd(gcd, n_iter=10, max_divisor=1024, max_fraction=128):
     for i in range(n_iter):
-        c = rand.randint(0, max_divisor)
-        a = c * rand.randint(0, max_fraction)
-        b = c * rand.randint(0, max_fraction)
+        c = rand.randint(1, max_divisor)
+        a = c * rand.randint(1, max_fraction)
+        b = c * rand.randint(1, max_fraction)
         assert gcd(a, a) == gcd(a, 0) == a
         assert gcd(b, b) == gcd(b, 0) == b
         assert gcd(a, 1) == gcd(b, 1) == 1
@@ -24,5 +24,12 @@ def gcd(n, m):
     return gcd(m, r)
 
 
+def gcd2(a, b):
+    while a and b:  # until a == 0 or b == 0
+        a, b = b % a, a
+    return max(a, b)  # if a == 0, return b; if b == 0, return a
+
+
 if __name__ == '__main__':
     test_gcd(gcd)
+    test_gcd(gcd2, max_divisor=100000)
