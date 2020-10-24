0d8139f7c6c88d4310fec0e4707eec4dcb243ec8
zuevval <valera.zuev.zva@gmail.com>
2020-10-24 20:39:46 +0300

    python:stu:search_chromosome: add more tests and visualize 'em

--- python/stu_labs/semester7/test_sv_search.py
+++ python/stu_labs/semester7/test_sv_search.py
@@ -1,23 +1,6 @@
-from typing import List, Tuple
-
 import pytest
-from dataclasses import dataclass
-
 from .sv_search import search_chr, search_chromosome
-
-
-@dataclass
-class Case:
-    name: str
-    sv_bounds: List[Tuple[int, int]]
-    k_conditions_pairs: List[Tuple[int, int]]
-    l_conditions_pairs: List[Tuple[int, int]]
-    indices_start_from_1: bool = True
-    solution_exists: bool = True
-
-    def __str__(self) -> str:
-        return 'sv test / {}'.format(self.name)
-
+from .testing_utils import Case, check, visualize
 
 SEARCH_CHR_TEST_CASES = [
     Case(name="basic", sv_bounds=[(0, 2), (1, 9), (2, 4), (7, 8)],
@@ -31,7 +14,7 @@ SEARCH_CHROMOSOME_TEST_CASES = [
          sv_bounds=[(1, 3), (2, 10), (3, 5), (8, 9)],
          k_conditions_pairs=[(1, 2), (3, 4)],
          l_conditions_pairs=[(1, 3)]),
-    Case(name="no answer",
+    Case(name="no answer (noncontroversial conditions)",
          sv_bounds=[(1, 10), (1, 10), (1, 10), (1, 10)],
          k_conditions_pairs=[(1, 2), (3, 4)],
          l_conditions_pairs=[(1, 3)],
@@ -40,30 +23,24 @@ SEARCH_CHROMOSOME_TEST_CASES = [
          sv_bounds=[(1, 3), (1, 3), (1, 3), (1, 3), (4, 5)],
          k_conditions_pairs=[(1, 2), (3, 3)],
          l_conditions_pairs=[(1, 2)],
-         solution_exists=False)
+         solution_exists=False),
+    Case(name="no constraints",
+         sv_bounds=[(2, 5)],
+         k_conditions_pairs=[],
+         l_conditions_pairs=[]),
+    Case(name="K constraints only",
+         sv_bounds=[(2, 5), (5, 7)],
+         k_conditions_pairs=[(1, 1), (1, 2), (2, 2)],
+         l_conditions_pairs=[]),
+    Case(name="L constraints only",
+         sv_bounds=[(2, 5), (5, 7), (6, 7), (4, 6), (1, 3), (1, 4)],
+         k_conditions_pairs=[],
+         l_conditions_pairs=[(1, 3), (2, 4), (5, 6)]),
+    Case(name="one possible answer",
+         sv_bounds=[(1, 2), (2, 7), (9, 11), (12, 15), (12, 14), (7, 11), (9, 15)],
+         k_conditions_pairs=[(2, 7), (3, 4)],
+         l_conditions_pairs=[(1, 2), (4, 5), (3, 6)])
 ]
-
-
-def check_conditions(case: Case, chr_id: int) -> None:
-    min_possible_id = 1 if case.indices_start_from_1 else 0
-    max_possible_id = max(right_bound for _, right_bound in case.sv_bounds)
-    if not case.indices_start_from_1:
-        max_possible_id -= 1
-    assert min_possible_id <= chr_id <= max_possible_id
-    sv_truth_list = [True if l_bound <= chr_id <= r_bound else False for l_bound, r_bound in case.sv_bounds]
-    if case.indices_start_from_1:
-        sv_truth_list.insert(0, False)  # add dummy zeroth value
-    for ak, bk in case.k_conditions_pairs:
-        assert sv_truth_list[ak] or sv_truth_list[bk]
-    for al, bl in case.l_conditions_pairs:
-        assert not (sv_truth_list[al] and sv_truth_list[bl])
-
-
-def check(case: Case, answer: int) -> None:
-    if case.solution_exists:
-        check_conditions(case, answer)
-    else:
-        assert answer == -1
 
 
 @pytest.mark.parametrize('case', SEARCH_CHR_TEST_CASES, ids=str)
@@ -76,3 +53,4 @@ def test_search_chr(case: Case) -> None:
 def test_search_chromosome(case: Case) -> None:
     answer = search_chromosome(case.sv_bounds, case.k_conditions_pairs, case.l_conditions_pairs)
     check(case, answer)
+    visualize(case, answer, "./out/test_search_chromosome.txt")
---
+++ python/stu_labs/semester7/testing_utils.py
@@ -0,0 +1,85 @@
+from pathlib import Path
+from typing import List, Tuple, Union
+from dataclasses import dataclass
+
+
+@dataclass
+class Case:
+    name: str
+    sv_bounds: List[Tuple[int, int]]
+    k_conditions_pairs: List[Tuple[int, int]]
+    l_conditions_pairs: List[Tuple[int, int]]
+    indices_start_from_1: bool = True
+    solution_exists: bool = True
+
+    def __str__(self) -> str:
+        return 'sv test / {}'.format(self.name)
+
+
+def check_conditions(case: Case, chr_id: int) -> bool:
+    min_possible_id = 1 if case.indices_start_from_1 else 0
+    max_possible_id = max(right_bound for _, right_bound in case.sv_bounds)
+    if not case.indices_start_from_1:
+        max_possible_id -= 1
+    assert min_possible_id <= chr_id <= max_possible_id
+    sv_truth_list = [True if l_bound <= chr_id <= r_bound else False for l_bound, r_bound in case.sv_bounds]
+    if case.indices_start_from_1:
+        sv_truth_list.insert(0, False)  # add dummy zeroth value
+    res = True
+    for ak, bk in case.k_conditions_pairs:
+        res &= sv_truth_list[ak] or sv_truth_list[bk]
+    for al, bl in case.l_conditions_pairs:
+        res &= not (sv_truth_list[al] and sv_truth_list[bl])
+    return res
+
+
+def check(case: Case, answer: int) -> None:
+    if case.solution_exists:
+        assert check_conditions(case, answer)
+    else:
+        assert answer == -1
+
+
+def stringify(x: int) -> str:
+    """ assuming x < 100 for pretty formatting"""
+    res = str(x) + "|"
+    if x < 10:
+        res = "0" + res
+    return res
+
+
+def visualize(case: Case, answer: int, out_filename: Union[str, Path, None] = None) -> None:
+    assert case.indices_start_from_1, "we visualize only sets of SV where indices start from 1"
+    max_chr_idx = max(stop for _, stop in case.sv_bounds)
+    lines: List[str] = [
+        "\n--- " + str(case) + " ---",
+        "   " + "".join(["▓▓|" if check_conditions(case, idx) else "  |" for idx in
+                         range(1, max_chr_idx + 1)]) + " -> possible answers",
+        "   " + "".join([stringify(idx) for idx in range(1, max_chr_idx + 1)]) + " -> chromosome index"
+    ]
+    for sv_idx, sv in enumerate(case.sv_bounds):
+        sv_idx += 1
+        lines.append(stringify(sv_idx) + "".join(
+            ["██|" if sv[0] <= idx <= sv[1] else "  |" for idx in range(1, max_chr_idx + 1)]))
+    if answer < 1:
+        lines.append("answer: impossible")
+    else:
+        lines.append("ans" + "  |" * (answer - 1) + "▒▒|")
+    lines.append("K conditions (a or b):   " + str(case.k_conditions_pairs))
+    lines.append("L conditions (not(a&b)): " + str(case.l_conditions_pairs))
+
+    if out_filename:
+        out_filename = Path(out_filename)
+        out_filename.parent.mkdir(exist_ok=True)
+        with open(str(out_filename), "a", encoding="utf-8") as file_out:
+            file_out.writelines([line + "\n" for line in lines])
+    else:
+        for line in lines:
+            print(line)
+
+
+if __name__ == "__main__":
+    visualize(Case(name="basic",
+                   sv_bounds=[(1, 3), (2, 10), (3, 5), (8, 9)],
+                   k_conditions_pairs=[(1, 2), (3, 4)],
+                   l_conditions_pairs=[(1, 3)]), answer=4, out_filename="./out/visualize_example.txt")
