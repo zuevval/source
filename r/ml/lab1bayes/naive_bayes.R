0e46e9454e6b92f30316ef1ab791011e6687cae5
zuevval <valera.zuev.zva@gmail.com>
2020-11-13 22:49:47 +0300

    r:stu:ml: start lab1: add tic-tac-toe

---
+++ r/ml/lab1bayes/naive_bayes.R
@@ -0,0 +1,84 @@
+# Title     : Bayesian Classifier
+# Objective : exploring Naive Bayesian classifier implemented in `e1071` package. Credit: https://github.com/levutkin
+# Created by: Valerii Zuev
+# Created on: 11/13/2020
+
+library(here) # for relative paths
+library(types) # for type annotations in functions
+library(e1071) # for Bayesian classifier
+library(ggplot2) # for figures
+library(dplyr) # for `summarize`, `group_by`
+
+### Naive Bayesian (categorial data)
+
+data_subfolder <- "r/ml/lab1bayes" # relative path to the Git root
+# A contains 10 columns (9 features: V1-V9 + class: V10) = design matrix & vector of labels. All data is of type Factor
+A_raw <- read.table(here(data_subfolder, "tic_tac_toe_data.txt"), sep = ",", stringsAsFactors = TRUE)
+n <- dim(A_raw)[1] # number of rows in A = number of data samples
+
+seeds <- c(168278, 180183, 400990, 474721, 127023, 853003) # seeds for reporducibility. Generated at https://random.org
+per_seed_stat_names <- c("seed", "true_negatives", "false_positives", "false_negatives", "true_positives")
+
+bayes_tic_tac_toe <- function(seed = ? integer, train_data_fraction = ? double) {
+  set.seed(seed) # fixed seed for reporducibility
+  A_rand <- A_raw[order(runif(n)),] # shuffling data
+
+  n_train <- as.integer(n * train_data_fraction) # `train_data_fration` - training, other - testing
+  A_train <- A_rand[1:n_train,]
+  A_test <- A_rand[(n_train + 1):n,]
+
+  A_classifier <- naiveBayes(V10 ~ ., data = A_train) # building a hypothesis
+  A_predicted <- predict(A_classifier, A_test) # evaluating the hypothesis
+  return(table(A_predicted, A_test$V10) / as.integer(n * (1 - train_data_fraction)))
+}
+
+bayes_try_fraction <- function(train_data_fraction = ? double) {
+  per_seed_stats <- data.frame(matrix(ncol = length(per_seed_stat_names), nrow = 0, dimnames = list(NULL, per_seed_stat_names)))
+  for (seed in seeds) {
+    per_seed_stats[nrow(per_seed_stats) + 1,] <- c(seed, bayes_tic_tac_toe(seed, fraction))
+  }
+  return(per_seed_stats)
+}
+
+stats_names <- c("fraction", per_seed_stat_names)
+stats_df <- data.frame(matrix(ncol = length(stats_names), nrow = 0, dimnames = list(NULL, stats_names)))
+for (fraction in c(0.5, 0.6, 0.7, 0.8, 0.9)) {
+  per_seed_stats <- bayes_try_fraction(fraction)
+  per_seed_stats$fraction <- fraction
+  stats_df <- rbind(stats_df, per_seed_stats)
+}
+
+stats_grouped <- group_by(stats_df, fraction)
+half_confidence_interval <- function(x) 1.96 * sd(x) / sqrt(length(x))
+# TODO reduce code duplication
+stats_summary_tn <- summarise(stats_grouped,
+                              stat_mean = mean(true_negatives),
+                              stat_max = stat_mean + half_confidence_interval(true_negatives),
+                              stat_min = stat_mean - half_confidence_interval(true_negatives),
+                              stat_name = "true negatives rate")
+stats_summary_tp <- summarise(stats_grouped,
+                              stat_mean = mean(true_positives),
+                              stat_max = stat_mean + half_confidence_interval(true_positives),
+                              stat_min = stat_mean - half_confidence_interval(true_positives),
+                              stat_name = "true positives rate")
+stats_summary_fn <- summarise(stats_grouped,
+                              stat_mean = mean(false_negatives),
+                              stat_max = stat_mean + half_confidence_interval(false_negatives),
+                              stat_min = stat_mean - half_confidence_interval(false_negatives),
+                              stat_name = "false negatives rate")
+stats_summary_fp <- summarise(stats_grouped,
+                              stat_mean = mean(false_positives),
+                              stat_max = stat_mean + half_confidence_interval(false_positives),
+                              stat_min = stat_mean - half_confidence_interval(false_positives),
+                              stat_name = "false positives rate")
+stats_summary <- rbind(stats_summary_tn, stats_summary_tp, stats_summary_fn, stats_summary_fp)
+
+pd <- position_dodge(.01)
+ggplot(stats_summary, aes(x = fraction, y = stat_mean, colour = stat_name)) +
+  geom_errorbar(aes(ymin = stat_min, ymax = stat_max), width = .02, position = pd) +
+  geom_line(position = pd) +
+  geom_point(position = pd) +
+  xlab("train data fraction") +
+  ylab("true negatives") +
+  ggtitle("Average behaviour on test data with confidence intervals")
+
