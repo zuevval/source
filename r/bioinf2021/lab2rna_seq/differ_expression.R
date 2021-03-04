836a88258d2345c16916d453cfda66a181a1c330
zuevval <valera.zuev.zva@gmail.com>
2021-03-04 22:07:07 +0300

    r:bio2021:lab2rna part2: add enrichment analysis

--- r/bioinf2021/lab2rna_seq/differ_expression.R
+++ r/bioinf2021/lab2rna_seq/differ_expression.R
@@ -11,6 +11,10 @@ library(tidyverse) # dplyr, ggplot2, ...
 data_dir <- "r/bioinf2021/lab2rna_seq/dataset"
 abs_path <- function(path) here(data_dir, path) # path relative to the Git root
 
+# ------------------------------------------
+# --- Analyzing RNA-seq data with DESeq2 ---
+# ------------------------------------------
+
 coldata <- read.csv(abs_path("annot.csv"), row.names = 1) %>% mutate_all(as.factor)
 counts <- read.csv(abs_path("GSE74737_counts.txt"), sep = "\t", row.names = "Gene") %>%
   select(rownames(coldata)) %>% # leave only columns mentioned in `annot.csv`
@@ -18,12 +22,27 @@ counts <- read.csv(abs_path("GSE74737_counts.txt"), sep = "\t", row.names = "Gen
 
 all(rownames(coldata) == colnames(counts)) # Performing a sanity check. Must be TRUE
 
-dds <- DESeqDataSetFromMatrix(countData = counts, colData = coldata, design = ~condition)
+dds <- DESeqDataSetFromMatrix(countData = counts, colData = coldata, design = ~ condition)
 dds_analysed <- DESeq(dds)
 res <- results(dds_analysed)
-head(res$padj) # list some adjusted p-values
+pval_thresh <- .2
+diff_expr <- res %>% na.omit %>% as.data.frame %>% filter(padj < pval_thresh) %>% rownames # differentially expressed genes
+# same as in the article: "ARL6", "ELK3", "MTHFSD"
 
-resultsNames(dds_analysed) # lists the coefficients
-res_lfc <- lfcShrink(dds_analysed, coef = "condition_.map_vs_.control", type = "apeglm")
-plotMA(res, ylim = c(-2, 2))
-plotMA(res_lfc, ylim = c(-2, 2))
+write.csv(diff_expr, abs_path("diff_expr.csv"))
+
+#resultsNames(dds_analysed) # lists the coefficients
+#res_lfc_ma <- lfcShrink(dds_analysed, coef = "condition_.map_vs_.control", type = "apeglm")
+#res_lfc_map <- lfcShrink(dds_analysed, coef = "condition_.ma_vs_.control", type = "apeglm")
+#plotMA(res)
+#plotMA(res_lfc_ma)
+#plotMA(res_lfc_map)
+
+# -------------
+# --- FGSEA ---
+# -------------
+
+# BiocManager::install("fgsea")
+library(fgsea)
+res_sorted <- res %>% na.omit %>% as.data.frame %>% arrange(padj)
+es <- calcGseaStat(res_sorted$padj, 1:6)
