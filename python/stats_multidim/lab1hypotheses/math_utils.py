32623fd1e6d5ce989a7fecef7a6a88aa0319f6c6
zuevval <valera.zuev.zva@gmail.com>
2021-03-09 09:30:03 +0300

    python:stats_multidim:lab1: add gamma distribution

--- python/stats_multidim/lab1hypotheses/math_utils.py
+++ python/stats_multidim/lab1hypotheses/math_utils.py
@@ -3,7 +3,7 @@ from typing import Tuple, Union, Callable
 import matplotlib.pyplot as plt
 import numpy as np  # type: ignore
 from scipy.optimize import minimize, OptimizeResult
-from scipy.stats import chi2
+from scipy.stats import chi2, gamma
 
 
 def mean(x: np.array) -> float:
@@ -43,28 +43,38 @@ def exp_pdf(lamb: float, x: Union[float, np.array]) -> Union[float, np.array]:  
 
 def fisher_test(x: np.array, n_bins: int = 6) -> None:
     """
-    Fisher's test for exponential distribution
+    Fisher's test for exponential distribution & other dists
     """
     nu, bins = np.histogram(x, bins=n_bins)  # `bins`: intervals, `nu`: number of points in each bin
     bins = list(zip(bins[:-1], bins[1:]))  # `bins`: [b1, b2, b3, ...] -> [(b1, b2), (b2, b3), ...]
     n = len(x)
 
-    def loss(param: float, theor_cdf: Callable[[float, float], float]) -> float:
+    def loss_1param(param: float, theor_cdf: Callable[[float, float], float]) -> float:
         # minimized function: X_n^2 = sum((nu_j - n p_j^0)^2/(n p_j^0))
         probabs = np.array([theor_cdf(r, param) - theor_cdf(l, param) for l, r in bins]).T  # array of p_j^0
         return np.sum((nu - n * probabs) ** 2 / (n * probabs))
 
+    def loss_gamma(param: np.array, theor_cdf: callable) -> float:
+        probabs = np.array(
+            [theor_cdf(r, a=param[0], scale=param[1]) - theor_cdf(l, a=param[0], scale=param[1])
+             for l, r in bins]).T
+        return np.sum((nu - n * probabs) ** 2 / (n * probabs))
+
     lambda_init = 1 / mean(x)  # start with maximum likelihood lambda
     df_init = 1.02  # matlab: [phat,pci] = mle(x,'pdf',@(x,v)chi2pdf(x,v),'start',1.1)
+    a_init, b_init = 0.55, 1.7  # matlab: [phat,pci] = mle(x,'pdf',@(x,a,b)gampdf(x,a,b),'start', [2, .6])
 
-    for theor_cdf, param, label in ((exp_cdf, lambda_init, "exponential"), (chi2.cdf, df_init, "chi squared")):
+    for loss, theor_cdf, param, label in (
+            (loss_1param, exp_cdf, lambda_init, "exponential"), (loss_1param, chi2.cdf, df_init, "chi squared"),
+            (loss_gamma, gamma.cdf, np.array([a_init, b_init]), "gamma"),
+    ):
         print("\n" + label + " distribution:")
         res: OptimizeResult = minimize(loss, param, args=(theor_cdf,))
         print("parameter found by minimizing the loss function: " + str(res.x))
 
         # when n -> inf, res.x ~ chi_square(N - r - 1), r = 1 - number of parameters (1 parameter: lambda)
         print("value found by minimizing the loss function:" + str(res.fun))
-        degrees_of_freedom = n_bins - 2
+        degrees_of_freedom = n_bins - 2  # TODO -3 for gamma distribution
         for gamma_value in [.9, .95]:
             print("critical value for gamma={}: {}".format(gamma_value, chi2.ppf(gamma_value, df=degrees_of_freedom)))
         print("p-value:" + str(1 - chi2.cdf(x=res.fun, df=degrees_of_freedom)))
@@ -73,12 +83,16 @@ def fisher_test(x: np.array, n_bins: int = 6) -> None:
 def plot_pdf_cdf(x: np.array, n_bins=6) -> None:
     lamb = 1 / mean(x)
     deg_of_freedom = 1.02  # matlab: [phat,pci] = mle(x,'pdf',@(x,v)chi2pdf(x,v),'start',1.1)
+    gam_a, gam_b = 0.55, 1.7  # matlab: [phat,pci] = mle(x,'pdf',@(x,a,b)gampdf(x,a,b),'start', [2, .6])
+
     t = np.arange(start=0, stop=5, step=.02)
 
     dist_params = (
         (exp_pdf(lamb=lamb, x=t), exp_cdf(x=t, lamb=lamb), "exponential (lambda=%.2g)" % lamb),
         (chi2.pdf(x=t, df=deg_of_freedom), chi2.cdf(x=t, df=deg_of_freedom),
-         "chi square (DoF=%.2g)" % deg_of_freedom))
+         "chi square (DoF=%.2g)" % deg_of_freedom),
+        (gamma.pdf(x=t, a=gam_a, scale=gam_b), gamma.cdf(x=t, a=gam_a, scale=gam_b),
+         "gamma (a=%.2g, b=%.2g)" % (gam_a, gam_b)))
 
     plt.figure()
     for _, y_cdf, label in dist_params:
@@ -101,8 +115,8 @@ def plot_pdf_cdf(x: np.array, n_bins=6) -> None:
     for y_pdf, _, label in dist_params:
         plt.plot(t, y_pdf, label="theoretical PDF: " + label)
     counts, bins = np.histogram(x, bins=n_bins)
-    exp_pdf_values = dist_params[0][0]
-    plt.hist(bins[:-1], bins, weights=counts * np.max(exp_pdf_values) / np.max(counts),
+    bin_heights = counts / (sum(counts) * (bins[1] - bins[0]))
+    plt.hist(bins[:-1], bins, weights=bin_heights,
              label="histogram (normalized)", color="white", edgecolor="black")
     plt.legend()
     plt.savefig("data/pdf.png")
