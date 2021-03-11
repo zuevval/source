c43b1c0c3c30c7ee4e3f5b7b7c5393c07a1d4f93
zuevval <valera.zuev.zva@gmail.com>
2021-03-11 18:10:12 +0300

    tex:primat:bioinf2021: add lec7 with TODOs

--- r/bioinf2021/lab2rna_seq/differ_expression.R
+++ r/bioinf2021/lab2rna_seq/differ_expression.R
@@ -17,9 +17,17 @@ abs_path <- function(path) here::here(data_dir, path) # path relative to the Git
 # --- Analyzing RNA-seq data with DESeq2 ---
 # ------------------------------------------
 
-coldata <- read.csv(abs_path("annot.csv"), row.names = 1) %>% mutate_all(as.factor)
-counts <- read.csv(abs_path("GSE74737_counts.txt"), sep = "\t", row.names = "Gene") %>%
-  select(rownames(coldata)) %>% # leave only columns mentioned in `annot.csv`
+coldata <- read.csv(abs_path("annot_control_vs_meth.csv"), row.names = 1) %>% mutate_all(as.factor)
+
+counts_df <- read.csv(abs_path("GSE74737_counts.txt"), sep = "\t", row.names = "Gene") %>%
+  select(rownames(coldata))
+counts <- counts_df %>%
+  mutate(entrez = AnnotationDbi::mget(
+    rownames(counts_df),
+    AnnotationDbi::revmap(org.Hs.eg.db::org.Hs.egSYMBOL),
+    ifnotfound = NA)) %>% # https://stat.ethz.ch/pipermail/bioconductor/2009-April/027168.html
+  filter(!is.na(entrez)) %>%
+  select(-entrez) %>%
   as.matrix
 
 all(rownames(coldata) == colnames(counts)) # Performing a sanity check. Must be TRUE
@@ -28,17 +36,20 @@ dds <- DESeq2::DESeqDataSetFromMatrix(countData = counts, colData = coldata, des
 dds_analysed <- DESeq2::DESeq(dds)
 deseq_res <- DESeq2::results(dds_analysed)
 DESeq2::plotMA(deseq_res)
-
-res_sorted <- deseq_res %>%
-  na.omit %>%
-  as.data.frame %>%
-  arrange(padj)
-pval_thresh <- .2
-diff_expr <- res_sorted %>%
-  filter(padj < pval_thresh) %>%
-  rownames # differentially expressed genes. The following are the same as in the article: "ARL6", "ELK3", "MTHFSD"
-
-write.csv(diff_expr, abs_path("diff_expr.csv"))
+deseq_res_df <- as.data.frame(deseq_res)
+deseq_res_df$entrez <- AnnotationDbi::mget(rownames(deseq_res_df), AnnotationDbi::revmap(org.Hs.eg.db::org.Hs.egSYMBOL), ifnotfound = NA)
+pval_thresh <- .001
+diff_expr <- deseq_res_df %>% filter(pvalue < pval_thresh)
+devnull <- mapply(function(df = ? data.frame, filename = ? character) {
+  df %>%
+    select(entrez) %>%
+    unlist %>%
+    write.table(abs_path(paste0(filename, "_entrez.csv")), sep = ",", row.names = F, col.names = F, quote = F)
+  df %>%
+    rownames %>%
+    unlist %>%
+    write.table(abs_path(paste0(filename, "_symbols.csv")), sep = ",", row.names = F, col.names = F, quote = F)
+}, list(deseq_res_df, diff_expr), c("background", "diff_expr_meth_vs_control"))
 
 # --------------------------------
 # --- dowloading KEGG pathways ---
@@ -50,18 +61,18 @@ kegg_pathways <- clusterProfiler::download_KEGG("hsa")
 # ------------
 
 # --- fgsea ---
-res_sorted$entrez <- AnnotationDbi::mget(rownames(res_sorted), AnnotationDbi::revmap(org.Hs.eg.db::org.Hs.egSYMBOL), ifnotfound = NA) # https://stat.ethz.ch/pipermail/bioconductor/2009-April/027168.html
-res_sorted <- res_sorted[!is.na(res_sorted$entrez),]
+res_sorted <- deseq_res_df %>% arrange(padj)
 ranks <- res_sorted$padj
 names(ranks) <- res_sorted$entrez
 
 fgsea_res <- fgsea::fgsea(kegg_pathways, ranks, scoreType = "pos") # https://bioinformatics-core-shared-training.github.io/cruk-summer-school-2018/RNASeq2018/html/06_Gene_set_testing.nb.html#conduct-analysis
 
 # --- KEGG enrichment with clusterProfiler ---
-kk <- clusterProfiler::enrichKEGG(gene = res_sorted[diff_expr,]$entrez, organism = 'hsa') # `hsa` stands for Homo Sapiens
+kk <- clusterProfiler::enrichKEGG(gene = diff_expr$entrez, organism = 'hsa') # `hsa` stands for Homo Sapiens
 head(kk, n = 10) # only 3 found
+
 # --- GO enrichment with goseq ---
-res_sorted$genes_signif <- as.integer(res_sorted$padj < pval_thresh)
+res_sorted$genes_signif <- as.integer(res_sorted$pvalue < pval_thresh)
 genome_id <- "hg19"
 gene_type_id <- "geneSymbol"
 res_sorted$lengths <- goseq::getlength(rownames(res_sorted), genome = genome_id, id = gene_type_id)
