6593c59b88133b7e1dafd62b393453ee2502642c
zuevval <valera.zuev.zva@gmail.com>
2021-05-05 08:02:57 +0300

    tex:primat:bioinf2021:ab_lab2gwas: add report

--- r/bioinf2021/ab_lab2gwas/p_adjuster.R
+++ r/bioinf2021/ab_lab2gwas/p_adjuster.R
@@ -7,5 +7,6 @@ library(dplyr)
 mlm_res <- read.csv("mlm_results.txt", sep = "\t") %>%
   na.omit %>%
   select(p, Marker)
-mlm_res$padj <- p.adjust(mlm_res$p)
-xtable::xtable(head(mlm_res %>% arrange(padj)), display = list("d", "e", "s", "f"))
+mlm_res$padj <- p.adjust(mlm_res$p, method = "BH")
+xtable::xtable(head(mlm_res %>% arrange(padj), 3),
+               display = list("d", "e", "s", "f"))
