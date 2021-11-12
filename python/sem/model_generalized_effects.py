2e247d159be81b5768096e794cfbbe9edb636f60
zuevval <valera.zuev.zva@gmail.com>
2021-11-12 18:51:02 +0300

    python:sem: fix ModelGeneralizedEffects example

--- python/sem/model_generalized_effects.py
+++ python/sem/model_generalized_effects.py
@@ -8,13 +8,13 @@ def main():
     desc = ex.get_model()
     data, (k1, k2) = ex.get_data(random_effects=2, moving_average=True)
 
-    ef = [EffectStatic('group ', k1), EffectStatic('group ', k2), EffectMA('time ', 2)]
+    ef = [EffectStatic('group', k1), EffectStatic('group', k2), EffectMA('time', 2)]
     m = semopy.ModelGeneralizedEffects(desc, ef)  # TODO example from the article causes type mismatch warning!
-    r = m.fit(data)  # TODO err
+    r = m.fit(data)
 
     params = ex.get_params()
-    # mape = np.mean(semopy.utils.compare_results(m, params))  # TODO err
-    # print("MAPE: " + mape * 100)
+    mape = np.mean(semopy.utils.compare_results(m, params))
+    print("MAPE: " + str(mape * 100))
 
     #  "C:\Python39\python.exe" -m cProfile -o mgen_out.profile model_generalized_effects.py && "C:\Python39\python.exe" -m tuna mgen_out.profile
 
