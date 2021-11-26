fedbc25b8ae0493bab67c82255975278727ce0a0
zuevval <valera.zuev.zva@gmail.com>
2021-11-26 09:00:39 +0300

    python:sem: add another example from the article

--- python/sem/model_generalized_effects.py
+++ python/sem/model_generalized_effects.py
@@ -3,7 +3,25 @@ import semopy
 from semopy.effects import EffectMA, EffectStatic
 
 
-def main():
+def example_model():
+    ex = semopy.examples.example_article
+    desc, data = ex.get_model(), ex.get_data()
+    print(desc)
+    print(data.head())
+
+    m = semopy.Model(description=desc)
+    r = m.fit(data=data, obj="FIML")  # TODO fix in the article: method="FIML": err
+    print(r)
+    print(m.inspect())
+
+    params = ex.get_params()
+    mape = np.mean(semopy.utils.compare_results(m, params))
+    print("MAPE: " + str(mape * 100))
+
+    semopy.semplot(mod=m,filename="model.png", plot_covs=True)
+
+
+def example_generalized_effects():
     ex = semopy.examples.example_article
     desc = ex.get_model()
     data, (k1, k2) = ex.get_data(random_effects=2, moving_average=True)
@@ -15,6 +33,10 @@ def main():
     params = ex.get_params()
     mape = np.mean(semopy.utils.compare_results(m, params))
     print("MAPE: " + str(mape * 100))
+
+
+def main():
+    example_model()
 
     #  "C:\Python39\python.exe" -m cProfile -o mgen_out.profile model_generalized_effects.py && "C:\Python39\python.exe" -m tuna mgen_out.profile
 
