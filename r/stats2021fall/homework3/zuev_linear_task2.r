2996d9041e2bbf0f526aabb192bb7d7a1077c042
zuevval <valera.zuev.zva@gmail.com>
2021-12-17 08:32:57 +0300

    r:primat:stats2021fall:hw3: rewrite&fix ANOVA

--- r/stats2021fall/homework3/zuev_linear_task2.r
+++ r/stats2021fall/homework3/zuev_linear_task2.r
@@ -8,10 +8,10 @@ dd <- dd %>% mutate(a = as.factor(a), b = as.factor(b))
 
 # (a) ANOVA model
 
-dd %>%
+dd.summary <- dd %>%
   group_by(a, b) %>%
-  summarise(mean = mean(y), var = var(y)) %>%
-  write.csv("anova2.csv")
+  summarise(mean = mean(y), var = var(y))
+dd.summary %>% write.csv("anova2.csv")
 
 # (b) plot
 
@@ -22,20 +22,22 @@ dd %>% ggplot(aes(x = a, y = y, color = b, shape = b, group = b)) +
 
 # (c) residuals analysis
 
-# TODO not exactly?
-anova.add <- lm(y ~ a + b, data = dd)
-summary(anova.add)
-anova.add$residuals %>% hist(main = "ANOVA: residuals distribution")
+dd.joint <- left_join(dd, dd.summary) %>%
+  rename(group_mean = mean, group_var = var) %>%
+  mutate(residual = y - group_mean)
+
+dd.joint$residual %>% hist(main = "ANOVA: residuals distribution")
 
 # (d) Step-by-step ANOVA
 
-anova.a <- lm(y ~ a, data = dd)
-anova.b <- lm(y ~ b, data = dd)
-anova.0 <- lm(y ~ 1, data = dd)
+param.alpha2 <- .02 # significance level
 
-anova.models <- list(anova.add, anova.a, anova.b, anova.0)
-names(anova.models) <- c("mult", "add", "a", "b", "0")
+anova.add <- aov(y ~ a + b, data = dd)
+anova.a <- aov(y ~ a, data = dd)
+anova.b <- aov(y ~ b, data = dd)
+anova.0 <- aov(y ~ 1, data = dd)
 
+anova.models <- list("add" = anova.add, "a" = anova.a, "b" = anova.b, "0" = anova.0)
 lapply(anova.models, summary)
 
 # (e) choosing the best model
