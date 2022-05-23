29279e6e68cae5ed6b26b1cb90c7cd72ee27b326
zuevval <valera.zuev.zva@gmail.com>
2022-05-23 15:58:51 +0300

    r:stats2022spring:hw1:fix GLMs

--- r/stats2022hw/zuev_biostat2022_hw1_tables.R
+++ r/stats2022hw/zuev_biostat2022_hw1_tables.R
@@ -1,6 +1,7 @@
 # variant 4
 library(coin) # cmh_test
 library(tidyverse)
+library(xtable)
 
 data_path <- function(filename) here::here("r/stats2022hw", filename)
 
@@ -31,21 +32,34 @@ df.admitted <- data.frame("males" = df.male$yes, "females" = df.female$yes)
 chisq.test(df.admitted)
 
 df.comb.counts <- bind_rows(
-  data.frame("gender" = "male", "admitted" = "yes", "count" = df.male$yes),
-  data.frame("gender" = "male", "admitted" = "no", "count" = df.male$no),
-  data.frame("gender" = "female", "admitted" = "yes", "count" = df.female$yes),
-  data.frame("gender" = "female", "admitted" = "no", "count" = df.female$no)
-) %>% mutate(gender = as.factor(gender), admitted = as.factor(admitted))
+  data.frame("gender" = "male", "admitted" = "yes", "count" = df.male$yes, "department" = row.names(df.male)),
+  data.frame("gender" = "male", "admitted" = "no", "count" = df.male$no, "department" = row.names(df.male)),
+  data.frame("gender" = "female", "admitted" = "yes", "count" = df.female$yes, "department" = row.names(df.female)),
+  data.frame("gender" = "female", "admitted" = "no", "count" = df.female$no, "department" = row.names(df.female))
+) %>% mutate(gender = as.factor(gender), admitted = as.factor(admitted),
+             department = as.factor(department), count = as.integer(count))
+
+xtable(df.comb.counts, type = "latex") %>% print(include.rownames = F)
 
 crosstab <- xtabs(count ~ gender + admitted, df.comb.counts)
 chisq.test(crosstab)
-cmh_test(crosstab)
+
+crosstab.by.department <- xtabs(count ~ gender + admitted + department, df.comb.counts)
+cmh_test(crosstab.by.department)
 
 # ----
-# Task 3: logistic regression
+# Task 3: prepare data for regression
 # ----
 
-df.comb.4glm <- df.combined %>% mutate(gender = as.factor(gender), department = as.factor(department))
+#df.comb.4glm <- df.combined %>% mutate(gender = as.factor(gender), department = as.factor(department))
+df.comb.cases <- uncount(df.comb.counts, count)
+unique(df.comb.cases) %>%
+  head(4) %>%
+  xtable(type = "latex")
+
+# ----
+# Task 4: logistic regression
+# ----
 
 formulas <- c(
   admitted ~ 1,
@@ -55,27 +69,43 @@ formulas <- c(
   admitted ~ gender * department
 )
 
-models <- lapply(formulas, function(f) glm(f, data = df.comb.4glm, family = "binomial"))
-models
+models.aic <- lapply(formulas, function(f) glm(f, data = df.comb.cases, family = "binomial")$aic) %>% unlist
+data.frame("model" = as.character(formulas), "AIC" = models.aic) %>%
+  xtable(type = "latex") %>%
+  print(include.rownames = F)
 
 # ----
-# Task 4. Poisson
+# Task 5. Poisson
 # ----
 
-poiss.res <- glm(count ~ gender * admitted, data = df.comb.counts, family = "poisson")
-poiss.null <- glm(count ~ gender + admitted, data = df.comb.counts, family = "poisson")
-poiss.llr <- poiss.null$deviance - poiss.res$deviance
-poiss.llr.pvalue <- pchisq(poiss.llr, df = 3, lower.tail = F)
+poiss.general <- glm(count ~ gender * admitted * department, data = df.comb.counts, family = "poisson")
+poiss.homogenous <- glm(count ~
+                          gender * admitted +
+                            gender * department +
+                            admitted * department,
+                        data = df.comb.counts, family = "poisson")
+poiss.llr <- poiss.homogenous$deviance - poiss.general$deviance
+poiss.llr.pvalue <- pchisq(poiss.llr, df = poiss.homogenous$df.residual, lower.tail = F)
 poiss.llr.pvalue
 
-poiss.dependent <- glm(count ~ gender + admitted, data = df.comb.counts, family = "poisson")
-poiss.independent <- glm(count ~ admitted, data = df.comb.counts, family = "poisson")
-poiss.dependent.llr <- poiss.independent$deviance - poiss.dependent$deviance
-poiss.dependent.llr.pvalue <- pchisq(poiss.dependent.llr, df = 3, lower.tail = F)
-poiss.dependent.llr.pvalue
+poiss.independent <- glm(count ~ gender * department + admitted * department, data = df.comb.counts, family = "poisson")
+poiss.independent.llr <- poiss.independent$deviance - poiss.general$deviance
+poiss.independent.llr.pvalue <- pchisq(poiss.independent.llr, df = poiss.independent$df.residual, lower.tail = F)
 
 df.comb.counts %>%
   group_by(gender, admitted) %>%
   summarise(count = sum(count)) %>%
   ggplot(aes(x = gender, y = count, fill = admitted)) +
-  geom_bar(stat = "identity", position = position_dodge())
+  geom_bar(stat = "identity", position = position_dodge()) +
+  theme_minimal() +
+  ggtitle("Total count of admitted / not admitted across all departments")
+ggsave(data_path("total_counts.jpg"), scale = .3)
+
+# -----
+# print LaTeX
+# -----
+df.male %>%
+  merge(df.female, by = 0, suffixes = c(".male", ".female")) %>%
+  select(!Row.names) %>%
+  mutate_all(as.integer) %>%
+  xtable(type = "latex")
