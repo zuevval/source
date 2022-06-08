ed9039dd676e2119f8c53dd16e83938a3d035f9c
zuevval <valera.zuev.zva@gmail.com>
2022-06-08 18:39:31 +0300

    r:stats2022spring:hw2: fix

--- r/stats2022hw/hw2/zuev_hw2_part2.R
+++ r/stats2022hw/hw2/zuev_hw2_part2.R
@@ -73,18 +73,15 @@ yv_names <- paste0("Y_V", lv.visit)
 corr.all <- var(df.id_grouped[, names(df.id_grouped) %in% yv_names]) # YouT 2:21:15
 # Correlations in groups: A effect only
 for (ii in lv.a) {
-  vnm <- paste0("var.a", ii)
-  vnm_corr <- paste0("corr", ii)
+  vnm_corr <- paste0("corr.a", ii)
   df.ii <- df[df$A == ii,]
   df.ii.tab <- expand_tbl(df.ii)
   df.ii.yv <- df.ii.tab %>% select(starts_with("Y_V"))
-  assign(vnm, var(df.ii.yv))
   assign(vnm_corr, cor(df.ii.yv))
 }
-var.a0 %>% xtable::xtable()
-var.a1 %>% xtable::xtable()
-corr0 # корреляции внутри групп по значениям фактора A (без учёта фактора B)
-corr1 # видим, что корреляции довольно слабые, но при A=1 чуть выше и везде положительные
+
+corr.a0 %>% xtable::xtable() # корреляции внутри групп по значениям фактора A (без учёта фактора B)
+corr.a1 %>% xtable::xtable() # видим, что корреляции довольно слабые, но при A=1 чуть выше и везде положительные
 
 for (ia in lv.a)
   for (ib in lv.b) {
@@ -94,13 +91,13 @@ for (ia in lv.a)
              filter(A == ia, B == ib) %>%
              expand_tbl %>%
              select(starts_with("Y_V")) %>%
-             var(use = "pairwise.complete.obs")
+             cor(use = "pairwise.complete.obs")
     )
   }
 
 cor_a0_b1 %>% xtable::xtable() # ковариационные характеристики при усилении группировки обычно не возрастают (в вероятностном смысле)
 cor_a0_b2 %>% xtable::xtable() # Если зависимость однородная, они и не падают
-cor_a0_b3 # конечно, основное, что хотим посмотреть - насколько расходится с моделью независимости
+cor_a0_b3 %>% xtable::xtable() # конечно, основное, что хотим посмотреть - насколько расходится с моделью независимости
 cor_a1_b1 %>% xtable::xtable()
 
 # variogram
@@ -111,11 +108,11 @@ plot(lme.variogram) # 2:45:16
 lme.variogram.resp <- Variogram(lme_model, form = ~Visit | ID, resType = "response") # остатки не нормированные
 plot(lme.variogram.resp) # end ~ 2:51:16
 
-# estimating covariance matrices using variograms. !! only for the group A == 0
+# estimating correlation matrices using variograms. !! only for the group A == 0
 stddevs.by.visit <- lv.visit %>%
   lapply(function(iv) {
-    df.filtered <- filter(df, A == 0)
-    var(df.filtered$Y[df.filtered$Visit == iv])
+    df.filtered <- filter(df, A == 0, Visit == iv)
+    var(df.filtered$Y)
   }) %>%
   unlist %>%
   sqrt
@@ -128,64 +125,151 @@ for (i in seq_along(lv.visit))
       stddevs.by.visit[j] *
       (1 - lme.variogram$variog[abs(i - j)])
   }
-var.residuals # end ~ 2:57:22
+var.residuals %>% xtable::xtable()  # end ~ 2:57:22
 
 # ----
 # task 4. Mixed linear model with ID effect
 # ----
+fixed_parts <- list(
+  Y ~ A * B * Visit,
+  Y ~ A * Visit + B * Visit + A * B,
+  Y ~ A * Visit + B * Visit,
+  Y ~ A * Visit + A * B,
+  Y ~ B * Visit + A * B,
+  Y ~ A + B + Visit,
+  Y ~ A + B,
+  Y ~ A + Visit,
+  Y ~ B + Visit,
+  Y ~ Visit,
+  Y ~ A,
+  Y ~ B,
+  Y ~ 1
+)
 
-df.lme <- function(rand_part = ? formula) lme(fixed = Y ~ Visit, random = rand_part, data = df, method = "ML")
-lme.simple <- df.lme(rand_part = ~1 | ID)
-lme.linear <- df.lme(rand_part = ~Visit | ID)
+no_rand_effects <- lapply(fixed_parts, function(fp) lm(fp, data = df))
+no_rand.aic <- lapply(no_rand_effects, AIC)
 
-cAIC(lme.simple)
-cAIC(lme.linear)
-lm(Y ~ Visit, data = df) %>% AIC # 7284.355
+df.lme.1_id <- list(
+  lme(fixed = Y ~ A * B * Visit, random = ~1 | ID, data = df, method = "ML"),
+  lme(fixed = Y ~ A * Visit + B * Visit + A * B, random = ~1 | ID, data = df, method = "ML"),
+  lme(fixed = Y ~ A * Visit + B * Visit, random = ~1 | ID, data = df, method = "ML"),
+  lme(Y ~ A * Visit + A * B, random = ~1 | ID, data = df, method = "ML"),
+  lme(Y ~ B * Visit + A * B, random = ~1 | ID, data = df, method = "ML"),
+  lme(Y ~ A + B + Visit, random = ~1 | ID, data = df, method = "ML"),
+  lme(Y ~ A + B, random = ~1 | ID, data = df, method = "ML"),
+  lme(Y ~ A + Visit, random = ~1 | ID, data = df, method = "ML"),
+  lme(Y ~ B + Visit, random = ~1 | ID, data = df, method = "ML"),
+  lme(Y ~ Visit, random = ~1 | ID, data = df, method = "ML"),
+  lme(Y ~ A, random = ~1 | ID, data = df, method = "ML"),
+  lme(Y ~ B, random = ~1 | ID, data = df, method = "ML"),
+  lme(Y ~ 1, random = ~1 | ID, data = df, method = "ML")
+)
+
+df.lme.var_id <- list(
+  lme(fixed = Y ~ A * B * Visit, random = ~Visit | ID, data = df, method = "ML"),
+  lme(fixed = Y ~ A * Visit + B * Visit + A * B, random = ~Visit | ID, data = df, method = "ML"),
+  lme(fixed = Y ~ A * Visit + B * Visit, random = ~Visit | ID, data = df, method = "ML"),
+  lme(Y ~ A * Visit + A * B, random = ~Visit | ID, data = df, method = "ML"),
+  lme(Y ~ B * Visit + A * B, random = ~Visit | ID, data = df, method = "ML"),
+  lme(Y ~ A + B + Visit, random = ~Visit | ID, data = df, method = "ML"),
+  lme(Y ~ A + B, random = ~Visit | ID, data = df, method = "ML"),
+  lme(Y ~ A + Visit, random = ~Visit | ID, data = df, method = "ML"),
+  lme(Y ~ B + Visit, random = ~Visit | ID, data = df, method = "ML"),
+  lme(Y ~ Visit, random = ~Visit | ID, data = df, method = "ML"),
+  lme(Y ~ A, random = ~Visit | ID, data = df, method = "ML"),
+  lme(Y ~ B, random = ~Visit | ID, data = df, method = "ML"),
+  lme(Y ~ 1, random = ~Visit | ID, data = df, method = "ML")
+)
+
+lm.pv <- function(lm_simple, lm_general) {
+  anova.res <- anova(lm_general, lm_simple)
+  anova.res[["p-value"]][2]
+}
+
+lm.1_id.pvals <- mapply(lm.pv, no_rand_effects, df.lme.1_id)
+lm.var_id.pvals <- mapply(lm.pv, no_rand_effects, df.lme.var_id)
+lm.1_vs_var_id.pvals <- mapply(lm.pv, df.lme.1_id, df.lme.var_id)
+
+lm.1_id.caic <- lapply(df.lme.1_id, function(model) cAIC(model)$caic)
+lm.var_id.caic <- lapply(df.lme.var_id, function(model) cAIC(model)$caic)
+
+lm.results <- data.frame("fixed part" = as.character(fixed_parts),
+                         "AIC" = unlist(no_rand.aic),
+                         "cAIC (1|ID)" = unlist(lm.1_id.caic),
+                         "cAIC (Visit|ID)" = unlist(lm.var_id.caic),
+                         "p-value: no random VS (1|ID)" = lm.1_id.pvals,
+                         "p-value: (1|ID) VS (Visit|ID)" = lm.1_vs_var_id.pvals)
+
+lm.results %>%
+  xtable::xtable() %>%
+  print(include.rownames = F)
+
 
 # ----
 # task 5. Mixed linear model with Visit (time) effect
 # ----
 
-lme.ab <- lmer(Y ~ A + B + (A | Visit), data = df, REML = F)
-lmerTest::ranova(lme.ab)
-
-lme.b.additive <- function(data) lmer(Y ~ B + Visit + (B | Visit), data = data, REML = F)
-lme.b.general <- function(data) lmer(Y ~ B * Visit + (B | Visit), data = data, REML = F)
+lme.b.additive <- function(data) lmer(Y ~ B + Visit + (Visit | ID), data = data, REML = F)
+lme.b.general <- function(data) lmer(Y ~ B * Visit + (Visit | ID), data = data, REML = F)
 dev.null <- levels(df$A) %>% lapply(function(a.lvl = ? factor) {
   print(paste("A =", a.lvl))
   data <- df %>% filter(A == a.lvl)
-  data %>% lme.b.additive %>% AIC %>% print # TODO check the hypothesis
-  data %>% lme.b.general %>% AIC %>% print
+  lm.add <- data %>% lme.b.additive
+  lm.general <- data %>% lme.b.general
+
+  anova(lm.add, lm.general) %>% print
+  #%>% AIC %>% print
 })
 
-lmer(Y ~ A + B + Visit + (A + B | Visit), data = df, REML = F) %>% AIC
-lmer(Y ~ A * Visit + A * B + (A + B | Visit), data = df, REML = F) %>% AIC
+lme.ab <- lmer(Y ~ A + B + Visit + (Visit | ID), data = df, REML = F)
+lme.b <- lmer(Y ~ B + Visit + (Visit | ID), data = df, REML = F)
+anova(lme.b, lme.ab)
+
 
 # ----
 # Task 6
 # ----
 
-gee::gee(Y ~ A + B, data = df, id = ID)
+library(geepack)
+
+gee.models <- fixed_parts %>% lapply(function(fp) geeglm(fp, id = ID, data = df, corstr = "unstructured"))
+gee.anova <- gee.models[2:length(gee.models)] %>% lapply(function(simple.model) anova(simple.model, gee.models[[1]])$`P(>|Chi|)`)
+
+gee.results <- data.frame(
+  "model" = as.character(fixed_parts[2:length(fixed_parts)]),
+  "p-value" = unlist(gee.anova)
+)
+
+gee.results %>%
+  xtable::xtable() %>%
+  print(include.rownames = F)
 
 # ----
 # Task 7
 # ----
 
 df.Visit2 <- df %>% mutate(Visit2 = Visit^2)
-lm.polynom <- lmer(Y ~ Visit + Visit2 + (A + B | Visit), data = df.Visit2, REML = F)
-lm.linear <- lmer(Y ~ Visit + (A + B | Visit), data = df)
-lmerTest::ranova(lm.polynom)
-lmerTest::ranova(lm.linear)
+lm.polynom <- lmer(Y ~ Visit + Visit2 + (1 | ID), data = df.Visit2, REML = F)
+lm.linear <- lmer(Y ~ Visit + (1 | ID), data = df.Visit2, REML = F)
+anova(lm.polynom, lm.linear)
 
 # ----
 # Task 8
 # ----
 
-# AIC, BIC: already done in (5)
+lm.1_id.bic <- lapply(df.lme.1_id, function(model) BIC(model))
+
+data.frame("model" = as.character(fixed_parts),
+           "cAIC" = unlist(lm.1_id.caic),
+           "BIC" = unlist(lm.1_id.bic)) %>%
+  xtable::xtable() %>%
+  print(include.rownames = F)
+
+df.lme.1_id[[8]]
 
 # ----
 # Task 9
 # ----
 
-lmer(Y ~ Visit + (B | Visit), data = df, REML = F)
-# TODO confints
+lm.Visit2 <- lme(Y ~ Visit + Visit2, random = ~Visit | ID, data = df.Visit2) # best = lm.polynom
+intervals(lm.Visit2, which = "fixed")
