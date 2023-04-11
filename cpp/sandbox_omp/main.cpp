bf16979c7dbaf59c96a2a0a54ea87d032b7ad1f9
Valerii Zuev <Valerii_Zuev@epam.com>
2023-04-11 08:50:55 +0300

    cpp:omp: add sandbox

---
+++ cpp/sandbox_omp/main.cpp
@@ -0,0 +1,52 @@
+#include <omp.h>
+#include <iostream>
+
+void testParallel() {
+#pragma  omp parallel default(none) shared(std::cout)
+    {
+        std::cout << omp_get_thread_num() << " out of " << omp_get_num_threads() << std::endl;
+    }
+}
+
+void testParallelSingle() {
+#pragma omp parallel default(none) shared(std::cout)
+#pragma omp single
+    {
+        std::cout << "single: " << omp_get_thread_num() << " out of " << omp_get_num_threads() << std::endl;
+    }
+}
+
+void testTasks() { // https://openmp.org/wp-content/uploads/sc13.tasking.ruud.pdf
+    for (auto _ = 0; _ < 10; ++_)
+#pragma omp parallel default(none) shared(std::cout)
+#pragma omp single
+    {
+        std::cout << "A ";
+#pragma omp task default(none) shared(std::cout)
+        std::cout << "car ";
+#pragma omp task default(none) shared(std::cout)
+        std::cout << "race ";
+#pragma omp taskwait
+        std::cout << "is fun to watch!" << std::endl;
+    }
+}
+
+void testTasksDepend() {
+    int x = 0;
+#pragma omp parallel default(none) shared(std::cout, x)
+#pragma omp single
+    {
+#pragma omp task default(none) shared(std::cout, x) depend(inout: x)
+        std::cout << x << std::endl; // End Of Line might be printed at the same time or after "x" is incremented
+
+#pragma omp task default(none) shared(std::cout, x)  depend(in: x)
+        x++;
+    };
+}
+
+int main() {
+//    testParallel();  // just prints a mess
+//    testParallelSingle();  // a single print
+//    testTasks(); // prints "car" and "race" in arbitrary order
+    testTasksDepend();  // prints 0; without dependencies specified, prints 0 or 1
+}
