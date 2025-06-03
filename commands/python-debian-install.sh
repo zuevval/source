56e63aa05b498e1b441a8c0cd7c2f78d89b86ffe
Valerii Zuev <valera.zuev.zva@gmail.com>
2025-06-03 16:53:11 +0300

    Create python-debian-install.sh

---
+++ commands/python-debian-install.sh
@@ -0,0 +1,2 @@
+sudo aptitude install build-essential libncursesw5-dev libreadline-gplv2-dev libssl-dev libsqlite3-dev tk-dev libc6-dev libbz2-dev libffi-dev -y
+wget https://www.python.org/ftp/python/3.11.3/Python-3.11.3.tgz
